befcf365da3bba9172a6b132aa0d3340d9d35cab
rolika01 <rolandbajnay@gmail.com>
2022-03-07 12:48:13 +0100

    nevek deklarálása

---
+++ src/jmkk/Jmkk.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmkk;
+
+/**
+ *
+ * @author hallgato
+ */
+public class Jmkk {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nev1,nev2,nev3;
+    }
+    
+}
